2675bfa8f827291616119b56b2fcda98992bdb40
Crystal <crystalyeo123@hotmail.com>
2025-01-06 23:22:50 +1300

    REFACTOR: refactor code for api call in App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import { Outlet } from 'react-router-dom';
 import { Header, Footer } from './components';
+import axios from 'axios';
 import { useEffect } from 'react';
-import { fetchTrendingData } from './utils/api';
+import { useDispatch } from 'react-redux';
+import { setBannerData, setImageURL } from './store/movieSlice';
 
 function App() {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await fetchTrendingData();
-        console.log(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+  const dispatch = useDispatch();
 
-    fetchData();
+  const fetchTrendingData = async () => {
+    try {
+      const response = await axios.get('/trending/all/week');
+      console.log('response', response.data.results);
+      dispatch(setBannerData(response.data.results));
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  const fetchConfiguration = async () => {
+    try {
+      const response = await axios.get('/configuration');
+      console.log(response.data.images.secure_base_url + 'original');
+      dispatch(setImageURL(response.data.images.secure_base_url + 'original'));
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTrendingData();
+    fetchConfiguration();
   }, []);
 
   return (
-    <main className='text-3xl text-white'>
+    <main className='pb-14 lg:pb-0'>
       <Header />
-      <div className='pt-32'>
+      <div className='min-h-[90vh]'>
         <Outlet />
       </div>
       <Footer />

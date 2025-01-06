6dbb8d85c5421b772179bbfffabd769bdc6e7dc5
Crystal <crystalyeo123@hotmail.com>
2025-01-06 13:59:48 +1300

    import header and footer in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import { Header, Footer } from './components';
 
 function App() {
   return (
     <main className='text-3xl text-white'>
-      <Outlet />
+      <Header />
+      <div className='pt-32'>
+        <Outlet />
+      </div>
+      <Footer />
     </main>
   );
 }
